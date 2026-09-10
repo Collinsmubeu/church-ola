99655a39c5c97a3e03ab284ecc781f5e996af53c
Your Name <your_email@example.com>
2026-09-10 18:46:16 +0300

    cleanup: remove duplicate 403 page and auth/auth.ts

--- src/lib/auth/auth.ts
+++
@@ -1,7 +0,0 @@
-import NextAuth from "next-auth";
-import { authOptions } from "@/lib/auth/options";
-
-const nextAuthInstance = NextAuth(() => authOptions);
-
-export const handlers = nextAuthInstance.handlers;
-export const auth = nextAuthInstance.auth;
